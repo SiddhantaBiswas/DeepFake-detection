90e85c057a5f2eb4d26aaeaf67d2332cca0f6d7d
SiddhantaBiswas <siddhantafb@gmail.com>
2021-01-13 21:55:43 +0530

    Add files via upload

---
+++ classifiers.py
@@ -0,0 +1,74 @@
+def Meso4(input_shape):
+
+    model = Sequential()
+    
+    model.add(Conv2D(8, (3, 3), input_shape=input_shape, padding='same', activation = 'relu'))
+    model.add(BatchNormalization())
+    model.add(MaxPooling2D(pool_size=(2, 2), padding='same'))
+
+    model.add(Conv2D(8, (5, 5), padding='same', activation = 'relu'))
+    model.add(BatchNormalization())
+    model.add(MaxPooling2D(pool_size=(2, 2), padding='same'))
+
+    model.add(Conv2D(16, (5, 5), padding='same', activation = 'relu'))
+    model.add(BatchNormalization())
+    model.add(MaxPooling2D(pool_size=(2, 2), padding='same'))
+
+    model.add(Conv2D(16, (5, 5), padding='same', activation = 'relu'))
+    model.add(BatchNormalization())
+    model.add(MaxPooling2D(pool_size=(4, 4), padding='same'))
+
+    model.add(Flatten())
+    model.add(Dropout(0.5))
+    model.add(Dense(16))
+    model.add(LeakyReLU(alpha=0.1))
+    model.add(Dropout(0.5))
+    model.add(Dense(1, activation = 'sigmoid'))
+
+    return model, 'Meso4'
+
+
+
+def Meso4VGG16(input_shape):
+
+    model = Sequential()
+    
+    model.add(Conv2D(8, (3, 3), input_shape=input_shape, padding='same', activation = 'relu'))
+    model.add(Conv2D(8, (3, 3), padding='same', activation = 'relu'))
+    model.add(BatchNormalization())
+    model.add(MaxPooling2D(pool_size=(2, 2), padding='same'))
+
+    model.add(Conv2D(16, (3, 3), padding='same', activation = 'relu'))
+    model.add(Conv2D(16, (3, 3), padding='same', activation = 'relu'))
+    model.add(BatchNormalization())
+    model.add(MaxPooling2D(pool_size=(2, 2), padding='same'))
+
+    model.add(Conv2D(32, (3, 3), padding='same', activation = 'relu'))
+    model.add(Conv2D(32, (3, 3), padding='same', activation = 'relu'))
+    model.add(Conv2D(32, (3, 3), padding='same', activation = 'relu'))
+    model.add(BatchNormalization())
+    model.add(MaxPooling2D(pool_size=(2, 2), padding='same'))
+
+    model.add(Conv2D(64, (3, 3), padding='same', activation = 'relu'))
+    model.add(Conv2D(64, (3, 3), padding='same', activation = 'relu'))
+    model.add(Conv2D(64, (3, 3), padding='same', activation = 'relu'))
+    model.add(BatchNormalization())
+    model.add(MaxPooling2D(pool_size=(2, 2), padding='same'))
+
+    model.add(Conv2D(128, (5, 5), padding='same', activation = 'relu'))
+    model.add(Conv2D(128, (5, 5), padding='same', activation = 'relu'))
+    model.add(Conv2D(128, (5, 5), padding='same', activation = 'relu'))
+    model.add(BatchNormalization())
+    model.add(MaxPooling2D(pool_size=(2, 2), padding='same'))
+
+    model.add(Flatten())
+    model.add(Dropout(0.5))
+    model.add(Dense(32))
+    model.add(LeakyReLU(alpha=0.1))
+    model.add(Dropout(0.5))
+    model.add(Dense(16))
+    model.add(LeakyReLU(alpha=0.1))
+    model.add(Dropout(0.5))
+    model.add(Dense(1, activation = 'sigmoid'))
+
+    return model, 'Meso4VGG16'
